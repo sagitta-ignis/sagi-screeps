0ea550b4568fd130c1279b7684bf591e94cc480a
Sagitta <sagitta.ignis@gmail.com>
2021-04-25 21:21:18 +0200

    Update project tools

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 "use strict";
 
-import clear from "rollup-plugin-clear";
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
-import screeps from "rollup-plugin-screeps";
+import clear from 'rollup-plugin-clear';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import screeps from 'rollup-plugin-screeps';
 
 let cfg;
 const dest = process.env.DEST;
@@ -24,7 +24,7 @@ export default {
 
   plugins: [
     clear({ targets: ["dist"] }),
-    resolve(),
+    resolve({ rootDir: "src" }),
     commonjs(),
     typescript({tsconfig: "./tsconfig.json"}),
     screeps({config: cfg, dryRun: cfg == null})
